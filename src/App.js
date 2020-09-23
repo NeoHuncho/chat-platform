f5d8e35ea542f3d3f9e0a0502e778693e776aee5
NeoHuncho <william.guinaudie@gmail.com>
2020-09-23 22:42:04 +0200

    added the firebase implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth'
 
+import {useAuthState} from 'react-firebase-hooks/auth';
+import {useCollectionData} from 'react-firebase-hooks/firebase';
 import './App.css';
 
+firebase.initializeApp({
+  apiKey: "AIzaSyC6zKr3NGUTfLM_2wuWWagb5U1Kg2NhsH0",
+  authDomain: "chat-plaform.firebaseapp.com",
+  databaseURL: "https://chat-plaform.firebaseio.com",
+  projectId: "chat-plaform",
+  storageBucket: "chat-plaform.appspot.com",
+  messagingSenderId: "134221553507",
+  appId: "1:134221553507:web:5795ebc5d24b01ad98fd56",
+  measurementId: "G-M66M207DX8"
+})
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
 function App() {
   return (
     <div className="App">
-
+{user ? <ChatRoom />: <SignIn/>}
     </div>
   );
 }
